3a41e504902e949325cf3e39c1d7db5747294784
omkark45 <omkarak45@gmail.com>
2021-06-13 01:14:07 +0530

    :hammer: add: logout controller

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -1,16 +1,12 @@
 import express from 'express'
-import { SignIn, SignUp } from '../controllers/AuthController'
+import { Logout, SignIn, SignUp } from '../controllers/AuthController'
 import { SignInInput, SignUpInput } from '../validation'
 import { validate } from '../middlewares/ValidateMiddleware'
-import { requireAuth } from '../middlewares/AuthMiddleware'
 
 const router = express.Router()
 
 router.route('/sign-up').post(validate(SignUpInput), SignUp)
 router.route('/sign-in').post(validate(SignInInput), SignIn)
-router.route('/secret').get(requireAuth, (req, res) => {
-	res.json({
-		msg: 'Welcome',
-	})
-})
+router.route('/logout').post(Logout)
+
 export = router
